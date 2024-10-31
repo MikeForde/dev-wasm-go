c428daa4c0eb1dec68285b4ad9462e1337766ba7
MikeForde <mikeforde@ymail.com>
2024-10-31 12:01:30 +0000

    working version local vsc

--- frontend/main.go
+++ frontend/main.go
@@ -1,15 +1,17 @@
+//go:build js && wasm
+
 package main
 
 import (
-	"fmt"
+	"syscall/js"
 )
 
 func main() {
-	// Optional: Initialization code
-	fmt.Println("WASM TinyGo Initialized")
+	js.Global().Set("hello", js.FuncOf(hello))
+	select {} // Keep the main function running
 }
 
-//export hello
-func hello() {
-	fmt.Println("Hello from TinyGo WASM!")
+func hello(this js.Value, args []js.Value) interface{} {
+	js.Global().Get("alert").Invoke("Hello from TinyGo WASM!")
+	return nil
 }
